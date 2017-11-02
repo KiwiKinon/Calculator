5bd4c8dc3be6c522d7188ce0d14e88bdcdd72acc
Eva <eva.m.thomas@outlook.fr>
2017-11-02 21:33:22 +0100

    Add subtraction

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition(int a, int b)
+int addition (int a, int b)
 {
     return a+b;
+}
+
+int subtraction (int a, int b)
+{
+    return a-b;
 }
 
 int main()
 {
     printf("L'addition de a et b vaut : %i\n", addition(1,4));
+    printf("La soustraction de a et b vaut : %i\n", subtraction(1,4));
     return 0;
 }
